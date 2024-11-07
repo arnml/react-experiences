2b83a62c656bf373c69eadc4166e0afd7f5c9965
Arnold Moya <dev.arn.ml@gmail.com>
2024-11-07 15:21:59 -0300

    Add spline-landscape with shadcn

---
+++ spline-landscape/app/page.tsx
@@ -0,0 +1,64 @@
+import Spline from '@splinetool/react-spline/next';
+import styles from '@/components/Spline.module.css';
+import { Button } from "@/components/ui/button"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
+export default function Home() {
+  return (
+    <>
+      <header>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      </header>
+      <div className={styles.splineContainer}>
+        <Spline
+          scene="/scene.splinecode"
+          className={styles.splineBackground}
+        />
+        <div>
+          <div>content</div>
+          <Button>Click me</Button>
+        </div>
+      </div>
+      <footer>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
+
+      </footer>
+    </>
+  );
+}
